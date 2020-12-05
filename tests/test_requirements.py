73bd081c88deb550d4f7726c218cf3a02099de7d
leynier <leynier41@gmail.com>
2020-12-05 17:58:14 -0500

    Fix requirements tests

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,4 +1,5 @@
 import os
+from typing import Set
 
 
 def test_requirements():
@@ -9,7 +10,11 @@ def test_requirements():
         with open("./requirements_temp.txt") as temp:
             actual_lines = actual.readlines()
             temp_lines = temp.readlines()
+            actual_set: Set[str] = set()
+            temp_set: Set[str] = set()
             for actual_line, temp_line in zip(actual_lines, temp_lines):
                 actual_line = actual_line.split(";")[0].strip("\r\n").strip("\n")
                 temp_line = temp_line.split(";")[0].strip("\r\n").strip("\n")
-                assert actual_line == temp_line, "requirements.txt not updated"
+                actual_set.add(actual_line)
+                temp_set.add(temp_line)
+            assert actual_set == temp_set, "requirements.txt not updated"
